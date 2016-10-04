d864a04ec94560bb3a771db9ce540bf1bb5b89f1
DBlackKat <zhirui09400@gmail.com>
2016-10-04 09:14:26 +0800

    update code

--- scanner/MainActivity.cs
+++ scanner/MainActivity.cs
@@ -16,7 +16,6 @@ using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 using Android.Support.V7.App;
 using Android.Support.V4.Widget;
 using Android.Views;
-using Newtonsoft.Json;
 
 namespace scanner
 {
@@ -44,15 +43,14 @@ namespace scanner
 			var result = await scanner.Scan(options);
 			if (result != null)
 			{
-				function func = new function();
 				string id;
 				id = result.Text;
 				id = id.Remove(0, 2);
 				id = id.Remove(id.Length - 1);
 				//Toast.MakeText(this, id, ToastLength.Long).Show();
 				string url = "http://theblackcat102.nctu.me:5000/NCTU/api/v1.0/tasks/" + id.ToString();
-				JsonValue pay = await func.FetchStudentAsync(url);
-				if (func.GotPay(pay))
+				JsonValue pay = await FetchStudentAsync(url);
+				if (GotPay(pay))
 				{
 					Toast.MakeText(this, id + ",有繳學聯會費喔", ToastLength.Long).Show();
 				}
@@ -64,17 +62,37 @@ namespace scanner
 
 		private async void testBtn_Click(object sender, EventArgs e)
 		{
-			function func = new function();
 			var sqlLiteFilePath = GetFileStreamPath("") + "/db_user.db";
 			if (File.Exists(sqlLiteFilePath))
 			{
 				File.Delete(sqlLiteFilePath);
 			}
-			string response = func.createDB(sqlLiteFilePath);
+			string response = createDB(sqlLiteFilePath);
 			Toast.MakeText(this, response, ToastLength.Short).Show();
-
-			string result = await func.parseData("http://theblackcat102.nctu.me:5000/NCTU/api/v1.0/grade/108");
-
+			string TARGETURL = "http://theblackcat102.nctu.me:5000/NCTU/api/v1.0/tasks";
+				//Toast.MakeText(this, TARGETURL, ToastLength.Short).Show();
+			var handler = new HttpClientHandler();
+				//Toast.MakeText(this, "handler gen pass", ToastLength.Short).Show();
+				// ... Use HttpClient.            
+			var client = new HttpClient(handler);
+				//Toast.MakeText(this, "client gen pass", ToastLength.Short).Show();
+			string result = null;
+			var byteArray = Encoding.ASCII.GetBytes("theblackcat102:AF51FAF9BE961002");
+				//Toast.MakeText(this, "encode gen pass", ToastLength.Short).Show();
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+				//Toast.MakeText(this, "auth gen pass", ToastLength.Short).Show();
+				//Console.WriteLine("Successful!\n");
+			try
+			{
+				HttpResponseMessage ServerResponse = await client.GetAsync(TARGETURL);
+				HttpContent content = ServerResponse.Content;
+				result = await content.ReadAsStringAsync();
+				// ... Check Status Code                                
+			}
+			catch (HttpRequestException except)
+			{
+				Toast.MakeText(this, "client get failed:" + except.Message, ToastLength.Short).Show();
+			}
 			// ... Display the result.
 			ProgressDialog progress;
 			progress = ProgressDialog.Show(this, "Loading", "Please Wait...", true);
@@ -83,8 +101,27 @@ namespace scanner
 			{
 				if (result != null && result.Length >= 50)
 				{
-					string dbPath = GetFileStreamPath("") + "/db_user.db";
-					func.fillingDB(result, dbPath); 
+					var data = JsonValue.Parse(result);
+					var students = data["task"];
+					string name;
+					Console.WriteLine("parsed successfully");
+					try
+					{
+						string dbPath = GetFileStreamPath("") + "/db_user.db";
+						var db = new SQLiteConnection(dbPath);
+						foreach (JsonValue student in students)
+						{
+							name = student["name"];
+							byte[] utf8bytes = Encoding.UTF8.GetBytes(name);
+							name = Encoding.UTF8.GetString(utf8bytes, 0, utf8bytes.Length);
+							var stu = new Student { name = name, stuID = student["stuID"], pay = student["pay"], sex = student["sex"] };
+							db.Insert(stu);
+						}
+					}
+					catch (SQLiteException ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
 				}
 			}).ContinueWith(
 				t =>
@@ -170,7 +207,72 @@ namespace scanner
 
         /********This Area is for the ckick event for toolbar and its button*******/
 
+        public string insertDB( string name, string id, string pay,string sex)
+		{
+			try
+			{
+				string dbPath = GetFileStreamPath("") + "/db_user.db";
+				var db = new SQLiteConnection(dbPath);
+				var stu = new Student { name = name, stuID = id, pay = pay ,sex = sex};
+				db.Insert(stu);
+				return "success";
+			}
+			catch (SQLiteException ex)
+			{
+				return ex.Message;
+			}
+		}
+		private bool GotPay(JsonValue json)
+		{
+			JsonValue Pay = json["task"];
+			bool payment = Pay["pay"];
+			return payment;
+		}
+		public string createDB(string dbPath)
+		{
+			try
+			{
+				var db = new SQLiteConnection(dbPath);
+				db.CreateTable<Student>();
+				db.Close();
+				return "db create success";
+			}
+			catch (SQLiteException ex)
+			{
+				return ex.Message;
+			}
+		}
 
+		private async Task<JsonValue> FetchStudentAsync(string url)
+		{
+			// Create an HTTP web request using the URL:
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new System.Uri(url));
+			request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes("theblackcat102:AF51FAF9BE961002"));
+			request.ContentType = "application/json";
+			request.Method = "GET";
+			// Send the request to the server and wait for the response:
+			using (WebResponse response = await request.GetResponseAsync())
+			{
+				//Get a stream representation of the HTTP web response:
+				using (Stream stream = response.GetResponseStream())
+				{
+					// Use this stream to build a JSON document object:
+					JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+					//string reponse = "Response: " + jsonDoc.ToString();
+					//debug purpose
+					//Toast.MakeText(this, reponse, ToastLength.Long).Show();
+					// Return the JSON document:
+					return jsonDoc;
+				}
+			}
+			//private CredentialCache GetCredential(string url)
+			//{
+			//	ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+			//	CredentialCache credentialCache = new CredentialCache();
+			//	credentialCache.Add(new System.Uri(url), "Basic", new NetworkCredential("python", "python123"));
+			//	return credentialCache;
+			//}
+		}
 	}
 }
 
